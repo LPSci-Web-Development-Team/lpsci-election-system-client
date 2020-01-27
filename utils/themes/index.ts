e071cca0e7fccbe5796f7fd4311c89daabf6fae3
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-01-27 21:25:16 +0800

    setup themes

---
+++ utils/themes/index.ts
@@ -0,0 +1,7 @@
+// ANCHOR Theme
+import { createMuiTheme } from '@material-ui/core/styles';
+import { TYPOGRAPHY } from './typography';
+
+export const THEME = createMuiTheme({
+  typography: TYPOGRAPHY,
+});
---
+++ utils/themes/typography.ts
@@ -0,0 +1,10 @@
+export const TYPOGRAPHY = {
+  fontFamily: [
+    '"IBM Plex Sans"',
+    '-apple-system',
+    '"BlinkMacSystemFont"',
+    '"Helvetica"',
+    '"Arial"',
+    'sans-serif',
+  ].join(',')
+},
