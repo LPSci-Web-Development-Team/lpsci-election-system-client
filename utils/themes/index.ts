6435be32d66ca79b4349c673bca7f20970527306
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-01-28 16:52:34 +0800

    setup colors theme

--- utils/themes/index.ts
+++ utils/themes/index.ts
@@ -1,12 +1,18 @@
 // ANCHOR Base
-import { DarkTheme } from 'baseui';
+import { LightTheme } from 'baseui';
 
 // ANCHOR Theme
 import { BREAKPOINTS } from './breakpoints';
 import { TYPOGRAPHY } from './typography';
 
 export const THEME = {
+  colors: {
+    ...LightTheme.colors,
+  },
+  lighting: {
+    ...LightTheme.lighting,
+  },
   media: BREAKPOINTS,
   typography: TYPOGRAPHY,
-  name: 'lyon-theme',
+  name: 'election-theme',
 };
