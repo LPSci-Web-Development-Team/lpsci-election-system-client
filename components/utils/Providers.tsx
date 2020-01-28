93df5fc6f9f46a73b3a245bcb0622950b69c3150
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-01-28 17:24:57 +0800

    sign in scoped models

--- components/utils/Providers.tsx
+++ components/utils/Providers.tsx
@@ -1,6 +1,13 @@
+// ANCHOR React
+import * as React from 'react';
+
+// ANCHOR Models
+import { SignInFormInput } from 'models/scoped-models/sign-in/SigninFormInput';
+
 export const makeProviders = () => [
   /*
-  ANCHOR: Provider for the MenuToolbar
-  NOTE: Used for providing the list items for the menu toolbar
+  ANCHOR: Provider for the SignInForm
+  NOTE: Used for providing the input fields a value
   */
+  <SignInFormInput.Provider key="SignInFormInput" />,
 ];
---
+++ models/scoped-models/sign-in/SigninFormInput.tsx
@@ -0,0 +1,26 @@
+// ANCHOR React
+import * as React from 'react';
+
+// ANCHOR Model
+import createModel from '@lxsmnsyc/react-scoped-model';
+
+// ANCHOR Hooks
+import { useDebouncedState } from '@lpsci/utils/hooks/useDebouncedState';
+
+export const SignInFormInput = createModel(() => {
+  const [lrn, setLrn] = useDebouncedState('', 150);
+  const [password, setPassword] = useDebouncedState('', 150);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState();
+
+  return {
+    lrn,
+    setLrn,
+    password,
+    setPassword,
+    loading,
+    setLoading,
+    error,
+    setError,
+  };
+});
