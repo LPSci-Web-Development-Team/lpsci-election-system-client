a3ed38a9a73d2ed5aa917078cc992a67348fe1d5
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-01-30 23:06:09 +0800

    created scoped models for voting tabs

---
+++ models/scoped-models/voting/VotingTab.tsx
@@ -0,0 +1,17 @@
+// ANCHOR React
+import * as React from 'react';
+
+// ANCHOR Model
+import createModel from '@lxsmnsyc/react-scoped-model';
+
+export const VotingTab = createModel(() => {
+  const [activeTab, setActiveTab] = React.useState<any>('0');
+  const [activeTabNum, setActiveTabNum] = React.useState<any>(0);
+
+  return {
+    activeTab,
+    setActiveTab,
+    activeTabNum,
+    setActiveTabNum,
+  };
+});
