82e5d5a1a24c97a372b7e5a4b38c84f55ca32def
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-01-30 23:10:28 +0800

    type for voting tabs models

--- models/scoped-models/voting/VotingTab.tsx
+++ models/scoped-models/voting/VotingTab.tsx
@@ -5,8 +5,8 @@ import * as React from 'react';
 import createModel from '@lxsmnsyc/react-scoped-model';
 
 export const VotingTab = createModel(() => {
-  const [activeTab, setActiveTab] = React.useState<any>('0');
-  const [activeTabNum, setActiveTabNum] = React.useState<any>(0);
+  const [activeTab, setActiveTab] = React.useState<string>('0');
+  const [activeTabNum, setActiveTabNum] = React.useState<number>(0);
 
   return {
     activeTab,
