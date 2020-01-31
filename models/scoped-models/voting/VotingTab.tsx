068ec623e203f0c8a6d926a1b67ca8956565d1af
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-01-31 22:11:12 +0800

    Add scoped models for active votes

--- models/scoped-models/voting/VotingTab.tsx
+++ models/scoped-models/voting/VotingTab.tsx
@@ -3,15 +3,19 @@ import * as React from 'react';
 
 // ANCHOR Model
 import createModel from '@lxsmnsyc/react-scoped-model';
+import { ICandidate } from 'models/interface/Vote';
 
 export const VotingTab = createModel(() => {
   const [activeTab, setActiveTab] = React.useState<React.Key>('0');
   const [activeTabNum, setActiveTabNum] = React.useState<number>(0);
+  const [activeVote, setActiveVote] = React.useState<ICandidate>();
 
   return {
     activeTab,
     setActiveTab,
     activeTabNum,
     setActiveTabNum,
+    vote: activeVote,
+    setVote: setActiveVote,
   };
 });
