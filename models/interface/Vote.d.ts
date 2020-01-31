4b362a306d214b20da8e2d8a20ae6d183839e935
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-01-31 10:27:08 +0800

    refactor type definitions for candidate

---
+++ models/interface/Vote.d.ts
@@ -0,0 +1,26 @@
+export interface ICandidate {
+  firstName: string;
+  lastName: string;
+  party: IParty;
+  position: IPosition;
+}
+
+export enum IPosition {
+  President = 'President',
+  VicePresident = 'Vice President',
+  Secretary = 'Secretary',
+  Treauser = 'Treasurer',
+  Auditor = 'Auditor',
+  PeaceOfficer = 'Peace Officer',
+  PublicInformationOfficer = 'Public Information Officer',
+  GradeEightLevelRepresentative = 'Grade 8 Level Representative',
+  GradeNineLevelRepresentative = 'Grade 9 Representative',
+  GradeTenLevelRepresentative = 'Grade 10 Level Representative',
+  GradeElevenLevelRepresentative = 'Grade 11 Level Representative',
+  GradeTwelveLevelRepresentative = 'Grade 12 Level Representative',
+}
+
+export interface IParty {
+  partyName: string;
+  colorHex: string;
+}
