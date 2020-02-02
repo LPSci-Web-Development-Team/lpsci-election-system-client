5f9c3cc595c5ea32e7f1b2d3597beb059a6a05b1
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-02-03 02:05:16 +0800

    Add review page with heading

---
+++ components/review-vote/ReviewVote/index.tsx
@@ -0,0 +1,11 @@
+// ANCHOR React
+import * as React from 'react';
+
+// ANCHOR Components
+import { ReviewVoteHeading } from '../ReviewVoteHeading';
+
+export const ReviewVote = React.memo(() => (
+  <>
+    <ReviewVoteHeading />
+  </>
+));
---
+++ components/review-vote/ReviewVoteHeading/styles.ts
@@ -0,0 +1,10 @@
+import { BlockOverrides } from 'baseui/block';
+
+export const HEADING: BlockOverrides = {
+  Block: {
+    style: {
+      fontWeight: 700,
+      marginBottom: '20px',
+    },
+  },
+};
