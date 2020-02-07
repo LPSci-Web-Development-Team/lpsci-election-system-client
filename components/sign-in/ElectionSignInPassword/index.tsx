73f9c130cab147b0016eaa399592320ed5ee83e2
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-02-07 12:25:18 +0800

    Add password handler

--- components/sign-in/ElectionSignInPassword/index.tsx
+++ components/sign-in/ElectionSignInPassword/index.tsx
@@ -12,18 +12,22 @@ import { Input } from 'baseui/input';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLock } from '@fortawesome/free-solid-svg-icons';
 
+// ANCHOR Scoped Models
+import { SignUpFormInput } from '@lpsci/scoped-models/sign-up/SignUpFormInput';
+
 export const ElectionSignInInputPassword = React.memo(() => {
-  const MemoFontAwesome = useConstant(() => <FontAwesomeIcon icon={faLock} />);
+  const LockIcon = useConstant(() => <FontAwesomeIcon icon={faLock} />);
+
+  const password = SignUpFormInput.useSelector((state) => state.handler.password);
 
   return (
-    <FormControl
-      label="Password"
-    >
+    <FormControl label="Password">
       <Input
-        placeholder="*****"
+        startEnhancer={LockIcon}
+        onChange={password}
         type="password"
-        startEnhancer={MemoFontAwesome}
-        name="password"
+        placeholder="*******"
+        required
       />
     </FormControl>
   );
