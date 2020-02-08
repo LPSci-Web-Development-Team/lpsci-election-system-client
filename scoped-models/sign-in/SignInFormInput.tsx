99c0ad3a5f8105c273a7bb0fb3fee0834f6b644c
pastelcolors <localcomputer1@gmail.com>
2020-02-08 20:35:08 +0800

    Remove debounceState for sign in form input

--- scoped-models/sign-in/SignInFormInput.tsx
+++ scoped-models/sign-in/SignInFormInput.tsx
@@ -5,12 +5,11 @@ import * as React from 'react';
 import createModel from '@lxsmnsyc/react-scoped-model';
 
 // ANCHOR Hooks
-import { useDebouncedState } from 'utils/hooks/useDebouncedState';
 import { useConstant } from '@lpsci/utils/hooks/useConstant';
 
 export const SignInFormInput = createModel(() => {
-  const [lrn, setLrn] = useDebouncedState('', 150);
-  const [password, setPassword] = useDebouncedState('', 150);
+  const [lrn, setLrn] = React.useState('');
+  const [password, setPassword] = React.useState('');
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState<string | null>(null);
   const [validLrn, setValidLrn] = React.useState<boolean>(false);
